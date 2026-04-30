68a19c7577d3f769e6d98f265cbcbd2e82ddffda
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-04-30 13:45:14 +0200

    Install pre-commit and run it on all files (#2)

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from app.routers import docs
 
+from app.routers import docs
 
 app = FastAPI()
 
 
 app.include_router(docs.router)
+
+
 @app.get("/")
 async def root():
     return {"message": "Ok"}

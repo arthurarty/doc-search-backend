fb19ec30fe801d5e5c236bb30cb3aa15cdcbcb80
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-05-02 18:09:41 +0200

    Use uppercase for env variables

--- app/config.py
+++ app/config.py
@@ -8,10 +8,10 @@ class Settings(BaseSettings):
     """
 
     app_name: str = "Doc-Search"
-    gcp_service_account_path: str
-    gcp_storage_bucket_name: str
+    GCP_SERVICE_ACCOUNT_PATH: str
+    GCP_STORAGE_BUCKET_NAME: str
     signed_url_expiry_time: int = 15  # 15 minutes
-    cors_origins: str
+    CORS_ORIGINS: str
     SQLALCHEMY_DATABASE_URL: str
     VOYAGE_AI_KEY: str
 
--- app/services/service_instances.py
+++ app/services/service_instances.py
@@ -3,8 +3,8 @@ from app.config import settings
 from app.services.cloud_storage_service import CloudStorageService
 
 cloud_storage_service = CloudStorageService(
-    bucket_name=settings.gcp_storage_bucket_name,
+    bucket_name=settings.GCP_STORAGE_BUCKET_NAME,
     google_cloud_client=GoogleCloudClient(
-        gcp_credentials_path=settings.gcp_service_account_path
+        gcp_credentials_path=settings.GCP_SERVICE_ACCOUNT_PATH
     ),
 )

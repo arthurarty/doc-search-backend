a9bd84e39b652895231ae4596c2b3847b4be7ac3
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-05-05 22:00:49 +0200

    Remove unnecessary function

--- app/tasks/index_file.py
+++ app/tasks/index_file.py
@@ -10,13 +10,9 @@ from app.celery_app import celery_app
 
 @celery_app.task
 def process_document(doc_identifier: UUID):
-    asyncio.run(_process_document(doc_identifier))
-
-
-async def _process_document(doc_identifier: UUID):
     """
     Create embeddings for each page of a document and store them in the database.
     """
     from app.services.service_instances import embedding_service
 
-    await embedding_service.create_document_embeddings(doc_identifier)
+    asyncio.run(embedding_service.create_document_embeddings(doc_identifier))

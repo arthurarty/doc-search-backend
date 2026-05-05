be3280518995ff4aee6321497250b08bd6661040
Arthur Nangai <17021137+arthurarty@users.noreply.github.com>
2026-05-05 21:59:52 +0200

    Remove unused logic

--- app/tasks/index_file.py
+++ app/tasks/index_file.py
@@ -7,9 +7,6 @@ from uuid import UUID
 
 from app.celery_app import celery_app
 
-TEXT_SPLITTER_CHUNK_SIZE = 500
-TEXT_SPLITTER_CHUNK_OVERLAP = 50
-
 
 @celery_app.task
 def process_document(doc_identifier: UUID):
